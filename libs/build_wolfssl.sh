74e5203509c746fd049319d99d4f06a5a6866c2f
mgrojo <mgrojo@gmail.com>
2025-04-04 19:22:15 +0200

    Manage libs/wolfssl as an Alire subcrate
    This simplifies the management of the dependency and prepares wolfssl and
    coap_spark for publication on the Alire
    community index.

--- libs/build_wolfssl.sh
+++ libs/build_wolfssl.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# This script can be used to validate changes in WolfSSL befoe submitting a PR
 # Install WolfSSL in Debian systems with DTLS support.
 # See wolfssl/INSTALL for information on installing dependencies in other systems
 
@@ -9,4 +10,4 @@ cd wolfssl
 ./autogen.sh
 ./configure --enable-dtls --enable-dtls13 --enable-psk
 make
-# sudo make install
+make distcheck
