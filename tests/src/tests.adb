7130ba3ebc8039e5a9c6a44238e027bb4eb7db0f
mgrojo <mgrojo@gmail.com>
2025-04-04 21:33:14 +0200

    Upload unit test results  to Codecov
    - Update proof results and Alire configuration

--- tests/src/tests.adb
+++ tests/src/tests.adb
@@ -1,11 +1,20 @@
+with Ada.Command_Line;
 with AUnit.Reporter.Text;
+with AUnit.Reporter.XML;
 with AUnit.Run;
 with CoAP_Suite; use CoAP_Suite;
 
 procedure Tests is
    procedure Runner is new AUnit.Run.Test_Runner (Suite);
-   Reporter : AUnit.Reporter.Text.Text_Reporter;
+   Reporter     : AUnit.Reporter.Text.Text_Reporter;
+   XML_Reporter : AUnit.Reporter.XML.XML_Reporter;
 begin
-   Reporter.Set_Use_ANSI_Colors (True);
-   Runner (Reporter);
+   if Ada.Command_Line.Argument_Count = 1
+     and then Ada.Command_Line.Argument (1) = "--xml"
+   then
+      Runner (XML_Reporter);
+   else
+      Reporter.Set_Use_ANSI_Colors (True);
+      Runner (Reporter);
+   end if;
 end Tests;
