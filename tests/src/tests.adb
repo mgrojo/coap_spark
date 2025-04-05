364e3bc1b0fac31dc725fca444e4ecd5ea1780b5
mgrojo <mgrojo@gmail.com>
2025-04-05 18:05:09 +0200

    Honor https://no-color.org/ in aunit reporter

--- tests/src/tests.adb
+++ tests/src/tests.adb
@@ -1,20 +1,18 @@
-with Ada.Command_Line;
+with Ada.Environment_Variables;
 with AUnit.Reporter.Text;
-with AUnit.Reporter.XML;
 with AUnit.Run;
 with CoAP_Suite; use CoAP_Suite;
 
 procedure Tests is
    procedure Runner is new AUnit.Run.Test_Runner (Suite);
    Reporter     : AUnit.Reporter.Text.Text_Reporter;
-   XML_Reporter : AUnit.Reporter.XML.XML_Reporter;
+   -- Honor https://no-color.org/
+   Use_Colors   : constant Boolean :=
+      not (Ada.Environment_Variables.Exists ("NO_COLOR") and then
+           Ada.Environment_Variables.Value ("NO_COLOR") /= "");
 begin
-   if Ada.Command_Line.Argument_Count = 1
-     and then Ada.Command_Line.Argument (1) = "--xml"
-   then
-      Runner (XML_Reporter);
-   else
-      Reporter.Set_Use_ANSI_Colors (True);
-      Runner (Reporter);
-   end if;
+
+   Reporter.Set_Use_ANSI_Colors (Use_Colors);
+   Runner (Reporter);
+
 end Tests;
