77ade9f87325c408b3ee089de9e0db0d2e292ba7
mgrojo <mgrojo@gmail.com>
2025-08-30 19:19:59 +0200

    Avoid `warning: subprogram "Dump" has no effect`

--- server/src/coverage.adb
+++ server/src/coverage.adb
@@ -1,5 +1,5 @@
 package body Coverage
-   with SPARK_Mode
+   with SPARK_Mode => Off
 is
 
    -- A body is needed to allow having the Always_Terminates aspect in the spec.
--- server/src/coverage.ads
+++ server/src/coverage.ads
@@ -1,10 +1,10 @@
 package Coverage
-   with SPARK_Mode
+   with SPARK_Mode, Abstract_State => Report
 is
 
    -- Dumps the current coverage data to the gcov report files.
    -- This version does nothing, so the same code can be used without
    -- linking with the gcov library when coverage data is not collected.
-   procedure Dump with Always_Terminates;
+   procedure Dump with Global => Report, Always_Terminates;
 
 end Coverage;
